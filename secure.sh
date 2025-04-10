be96f5a500dab2c583a914cd8e38b758916d8d6e
oriole1 <nvme12@proton.me>
2025-04-10 08:39:12 -0400

    Update secure.sh

--- secure.sh
+++ secure.sh
@@ -4,4 +4,4 @@
   && echo 'd010cb9e0f3c644e9df3bfb387f42f7dbbffbbd481fb50c32683bbe71f994451  /tmp/hblock' | shasum -c \
   && sudo mv /tmp/hblock /usr/local/bin/hblock \
   && sudo chown 0:0 /usr/local/bin/hblock \
-  && sudo chmod 755 /usr/local/bin/hblock && hblock && sudo dnf install fail2ban &&  sudo systemctl start fail2ban && sudo systemctl enable fail2ban && sudo nano /etc/fail2ban/jail.local && sudo systemctl restart fail2ban && sudo dnf install clamav clamd klamav --skip-unavailable && sudo freshclam
+  && sudo chmod 755 /usr/local/bin/hblock && hblock && sudo dnf install fail2ban &&  sudo systemctl start fail2ban && sudo systemctl enable fail2ban && sudo nano /etc/fail2ban/jail.local && sudo systemctl restart fail2ban && sudo dnf install clamav clamd firewall-config --skip-unavailable && sudo freshclam
