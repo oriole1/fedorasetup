89b0f33e53b1c31ea6ff05ac23f5de959356a51e
oriole1 <nvme12@proton.me>
2025-04-10 07:32:51 -0400

    Update cliapps.sh

--- cliapps.sh
+++ cliapps.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && sudo dnf group install development-tools --skip-unavailable && brew install gcc 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && sudo dnf group install development-tools --skip-unavailable && brew install gcc 
 
