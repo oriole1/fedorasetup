d42f4ea7f990078259b7d0dce82e5ac6506b08b5
oriole1 <nvme12@proton.me>
2025-04-12 12:46:25 +0700

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -2,9 +2,9 @@
 
 sudo dnf update -y && \
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y && \
-sudo dnf install kernel-devel kernel-headers gcc make dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig -y && \
+sudo dnf install kernel-devel kernel-headers gcc make dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda pkgconfig -y && \
 echo -e "blacklist nouveau\noptions nouveau modeset=0" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf && \
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg && \
 sudo dracut --force /boot/initramfs-$(uname -r).img $(uname -r) && \
 sudo dnf remove xorg-x11-drv-nouveau -y && \
-sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda && reboot
+sudo reboot
