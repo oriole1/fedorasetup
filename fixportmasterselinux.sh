cf4d8d95427473adc719e7284d3e268a4bd19f64
oriole1 <nvme12@proton.me>
2025-04-13 16:31:51 +0700

    Update fixportmasterselinux.sh

--- fixportmasterselinux.sh
+++ fixportmasterselinux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo dnf install policycoreutils-python-utils && sudo semanage fcontext -a -t bin_t "/opt/safing/portmaster/portmaster-start" && sudo restorecon -v /opt/safing/portmaster/portmaster-start && sudo systemctl daemon-reload && systemctl restart portmaster 
+sudo dnf install policycoreutils-python-utils && sudo semanage fcontext -a -t bin_t "/opt/safing/portmaster/portmaster-start" && sudo restorecon -v /opt/safing/portmaster/portmaster-start && sudo systemctl daemon-reload && sudo systemctl enable portmaster && systemctl restart portmaster 
 #make sure to install portmaster first
