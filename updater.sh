50881d99e6660877dabcd8dd9e9fb265c3925cc6
oriole1 <nvme12@proton.me>
2025-04-17 19:27:09 +0700

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -4,4 +4,4 @@ sudo dnf update && sudo dnf upgrade && flatpak update && brew update && brew upg
   && echo 'd010cb9e0f3c644e9df3bfb387f42f7dbbffbbd481fb50c32683bbe71f994451  /tmp/hblock' | shasum -c \
   && sudo mv /tmp/hblock /usr/local/bin/hblock \
   && sudo chown 0:0 /usr/local/bin/hblock \
-  && sudo chmod 755 /usr/local/bin/hblock && hblock
+  && sudo chmod 755 /usr/local/bin/hblock && hblock && sudo freshclam
